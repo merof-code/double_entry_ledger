1de25ede0684721ecfcd10a49e01cc1b3c53bedd
merof <misha.rosh@gmail.com>
2024-07-10 00:47:18 +0000

    added more config. damn those are big files

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,6 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
+require "active_record"
+require "active_record/associations"
+require "active_support"
